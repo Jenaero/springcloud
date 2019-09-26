d0ecc574daabdb219a00f497f31ebf8f4bf7eb48
renyajian <renyajian@yf-fresh.com>
2019-09-27 02:20:07 +0800

    微服务 -> api模块 增加sdk

---
+++ microservicecloud-provider-dept-8001/dept.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS clouddb01;
+CREATE DATABASE clouddb01 CHARACTER SET UTF8;
+USE clouddb01;
+CREATE TABLE dept (
+  deptno    BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  dname     VARCHAR(60),
+  db_source VARCHAR(60)
+);
+
+INSERT INTO dept(dname,db_source) VALUES ("开发部",database());
+INSERT INTO dept(dname,db_source) VALUES ("人事部",database());
+INSERT INTO dept(dname,db_source) VALUES ("财务部",database());
+INSERT INTO dept(dname,db_source) VALUES ("市场部",database());
+INSERT INTO dept(dname,db_source) VALUES ("运维部",database());
+
+select * from clouddb01.dept;
