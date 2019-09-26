50817df4e406f295d89fe796384cc2fbbd4ef797
renyajian <renyajian@yf-fresh.com>
2019-09-27 00:55:27 +0800

    微服务 -> api模块 增加sdk

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -1,30 +1,29 @@
 package com.atguigu.springcloud.entities;
 
-import java.io.Serializable;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 
 @NoArgsConstructor
 //@AllArgsConstructor
 @Data
-@Accessors(chain=true)
-public class Dept implements Serializable// entity --orm--- db_table
-{
-	private Long 	deptno; // 主键
-	private String 	dname; // 部门名称
-	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
-	
-	public Dept(String dname)
-	{
-		super();
-		this.dname = dname;
-	}
-	
-	
+@Accessors(chain = true) //链式访问
+public class Dept implements Serializable {  //entity --orm--- db_table   必须序列化
+    private Long deptno; // 主键
+    private String dname; // 部门名称
+    private String db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+    public Dept(String dname) {
+        super();
+        this.dname = dname;
+    }
+
+    public static void main(String[] args) {
+        Dept dept = new Dept();
+        //链式访问
+        dept.setDb_source("rd").setDname("db01").setDeptno(1234L);
+    }
 }
